983a51b668e1d9f1ec952ed7375ed0961d523ef7
messier3331 <messier3331@naver.com>
2024-06-06 17:01:00 +0900

    잔버그 수정
    ㅇㅇ

--- FireTreeTrillion/Client/Private/BladeKnightSword.cpp
+++ FireTreeTrillion/Client/Private/BladeKnightSword.cpp
@@ -101,7 +101,7 @@ HRESULT CBladeKnightSword::Add_Components()
 	CHECK_FAILED(hr);
 
 	/* For.Com_Model */
-	hr = __super::Add_Component(TEXT(/*Prototype_Component_Model_BladeKnightSword*/"Prototype_Component_Model_KirbyWeapon_Sword"),
+	hr = __super::Add_Component(TEXT("Prototype_Component_Model_BladeKnightSword"),
 		TEXT("Com_Model"), (CComponent**)&m_pModelCom);
 	CHECK_FAILED(hr);
 
