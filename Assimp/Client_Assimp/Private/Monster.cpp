5415559e2673072baa814ad90c9e1c7494e162ba
ahnjh97 <ahnjh1997@naver.com>
2024-06-09 21:46:35 +0900

    Level_Intro 맵 텍스쳐 수정, BlendMap 추가

--- Assimp/Client_Assimp/Private/Monster.cpp
+++ Assimp/Client_Assimp/Private/Monster.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "Monster.h"
 
 CMonster::CMonster(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
--- FireTreeTrillion/Client/Private/Loader.cpp
+++ FireTreeTrillion/Client/Private/Loader.cpp
@@ -625,6 +625,7 @@ void CLoader::SetUp_ModelScaleRotation(LEVEL eLevel)
 		m_vecModelInfo.emplace_back("KirbyArmour_Sword", TYPE_NONANIM, 1.f);
 
 		m_vecModelInfo.emplace_back("Level0Stage1Step01", TYPE_NONANIM, 1.f, 0.f, 0, true);
+		m_vecModelInfo.emplace_back("Level0Stage1Step01_Blend", TYPE_NONANIM);
 		m_vecModelInfo.emplace_back("Trigger", TYPE_NONANIM, 0.01f);
 
 		// For Monster
