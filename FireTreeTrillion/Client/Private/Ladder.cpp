cc222e54862f522847b7db08af2495736ff6524b
messier3331 <messier3331@naver.com>
2024-06-14 16:24:45 +0900

    사다리 컬링 끔
    ㅇㅇ

--- FireTreeTrillion/Client/Private/Ladder.cpp
+++ FireTreeTrillion/Client/Private/Ladder.cpp
@@ -55,7 +55,7 @@ _int CLadder::Tick(_float fTimeDelta)
 
 void CLadder::Late_Tick(_float fTimeDelta)
 {
-	if (true == m_pGameInstance->isInFrustum_WorldSpace(m_pTransformCom->Get_State_Vector(CTransform::STATE_POSITION), 2.0f))
+	//if (true == m_pGameInstance->isInFrustum_WorldSpace(m_pTransformCom->Get_State_Vector(CTransform::STATE_POSITION), 2.0f))
 	{
 		m_pGameInstance->Add_RenderGroup(CRenderer::RENDER_NONBLEND, this);
 		m_pGameInstance->Add_RenderGroup(CRenderer::RENDER_SHADOW, this);
