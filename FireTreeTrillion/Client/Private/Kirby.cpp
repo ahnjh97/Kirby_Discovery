2d5e0c25889283c3bf9abd7ff687472d4d562094
messier3331 <messier3331@naver.com>
2024-06-17 12:21:20 +0900

    빌드 오류 수정
    ㅇㅇ

--- FireTreeTrillion/Client/Private/Kirby.cpp
+++ FireTreeTrillion/Client/Private/Kirby.cpp
@@ -379,11 +379,6 @@ void CKirby::Collision(CCollisionCenter::CONTENT_TYPE eContent, CPhysXObject* pO
 
 }
 
-void CKirby::Collision_Hitbox(CPhysXObject* pGameObject)
-{
-	m_iTestAnim++;
-}
-
 _float3 CKirby::Make_RepulsiveDir(CPhysXObject* pObject)
 {
 	_vector vPos = m_pTransformCom->Get_State_Vector(CTransform::STATE_POSITION);
