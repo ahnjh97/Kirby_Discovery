92f4ab1eb77b94052d071f9617ee7e1d2fc6a218
junsu8282 <ghkdlxmejtk@naver.com>
2024-06-17 16:03:37 +0900

    파리 버그 수정

--- FireTreeTrillion/Client/Private/Level_Intro.cpp
+++ FireTreeTrillion/Client/Private/Level_Intro.cpp
@@ -21,7 +21,7 @@ CLevel_Intro::CLevel_Intro(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 
 HRESULT CLevel_Intro::Initialize()
 {
-	m_pGameInstance->Set_RenderMode(CRenderer::MODE_GAMEPLAY);
+	m_pGameInstance->Set_RenderMode(CRenderer::MODE_TOOL);
 	//CLevelChanger::Get_Instance()->Load();
 
 	HRESULT hr;
