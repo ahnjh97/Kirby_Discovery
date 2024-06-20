4a91d419752c008d39a277f0190988d6a72b38ee
junsu8282 <ghkdlxmejtk@naver.com>
2024-06-20 20:20:38 +0900

    dkd

--- FireTreeTrillion/Client/Private/CappyBody_State.cpp
+++ FireTreeTrillion/Client/Private/CappyBody_State.cpp
@@ -105,6 +105,9 @@ void CCappyBody_Find_State::OnStateUpdate(CGameObject* pGameObject, _float fTime
 	CKirby* pKirby = static_cast<CKirby*>(m_pGameInstance->Get_GameObject(*m_pGameInstance->Get_CurrentLevelID(), TEXT("Layer_Player")));
 	CTransform* pKirbyTransformCom = pKirby->Get_TransformCom();
 
+	// ÀÚÀ¯ ³«ÇÏ
+	pController->FreeFall(pTransformCom, fTimeDelta, 6.f);
+
 	pCappyBody->Set_Render(false);
 
 	if (true == pCappyBody->IsAnimFinished())
@@ -290,7 +293,7 @@ void CCappyBody_Damage_State::OnStateUpdate(CGameObject* pGameObject, _float fTi
 		pCappy->Set_DamageJumpPower(fDamageJumpPower);
 
 
-		if (true == pCappy->IsAnimFinished() || pController->Is_Terrain())
+		if (/*true == pCappy->IsAnimFinished() || */pController->Is_Terrain())
 		{
 			if (pCappy->Get_Hp() <= 0.f)
 				pCappy->Set_Dead();
