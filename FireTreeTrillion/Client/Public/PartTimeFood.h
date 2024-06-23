f6eeb58a4c9a22ad5024ea4fda6ae50194c211dc
Jiyoung_YW'sASUS <cheeze2017@naver.com>
2024-06-23 20:11:05 +0900

    파트타이머와 파트타임헬퍼

--- FireTreeTrillion/Client/Public/PartTimeFood.h
+++ FireTreeTrillion/Client/Public/PartTimeFood.h
@@ -12,31 +12,42 @@ BEGIN(Client)
 
 class CPartTimeFood final : public CItemObject
 {
+public:
+	struct FOOD_DESC : public GAMEOBJECT_DESC
+	{
+		_float4x4* pBoneMatrix = { nullptr };
+	};
+
 private:
 	CPartTimeFood(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
 	CPartTimeFood(const CPartTimeFood& rhs);
 	virtual ~CPartTimeFood() = default;
 
 public:
-	virtual HRESULT		Initialize_Prototype()	override;
-	virtual HRESULT		Initialize(void* pArg)	override;
-	virtual _int		Tick(_float fTimeDelta) override;
-	virtual void		Late_Tick(_float fTimeDelta) override;
-	virtual HRESULT		Render()				override;
-	virtual HRESULT		Render_LightDepth()		override;
+	virtual HRESULT		Initialize_Prototype()			override;
+	virtual HRESULT		Initialize(void* pArg)			override;
+	virtual _int		Tick(_float fTimeDelta)			override;
+	virtual void		Late_Tick(_float fTimeDelta)	override;
+	virtual HRESULT		Render()						override;
+	virtual HRESULT		Render_LightDepth()				override;
 #ifdef _DEBUG
 	virtual void		Render_IMGUI() override;
 #endif
-	virtual void		Collision(CCollisionCenter::CONTENT_TYPE eContent, CPhysXObject* pObject) override;
+	void				Collision(CCollisionCenter::CONTENT_TYPE eContent, CPhysXObject* pObject) override;
 
+	void				Update_Position(_float4 vPos);
+	void				Set_Item(PARTTIME_ITEM _eItem) { m_eItem = _eItem; }
+	void				Set_Render(_bool _bRender) { m_bRender = _bRender; }
 private:
 	HRESULT				Add_Components();
 	HRESULT				Bind_ShaderResources();
 
 private:
+	CShader*			m_pShaderCom = { nullptr };
+	_float4x4*			m_pBoneMatrix = { nullptr };
 	array<CModel*, 4>	m_arrModelCom = { nullptr };
-	//CGameObject*		m_pPlayer = { nullptr };
 	PARTTIME_ITEM		m_eItem = { PARTTIME_ITEM::DRINK };
+	_bool				m_bRender = false;
 
 public:
 	static CPartTimeFood*	Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
