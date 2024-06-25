c664f716a034212a049c6de923c5a43d4e04cd4e
ahnjh97 <ahnjh1997@naver.com>
2024-06-25 22:21:29 +0900

    집

--- FireTreeTrillion/Client/Private/BreakableRock.cpp
+++ FireTreeTrillion/Client/Private/BreakableRock.cpp
@@ -163,9 +163,9 @@ HRESULT CBreakableRock::Bind_ShaderResources()
 		return E_FAIL;
 	if (FAILED(m_pShaderCom->Bind_RawValue("g_bMotionBlur", &m_bMotionBlur, sizeof(_bool))))
 		return E_FAIL;
-
 	if (FAILED(m_pShaderCom->Bind_RawValue("g_fWhiteColorDiffuse", &m_fWhiteColorDiffuse, sizeof(_float))))
 		return E_FAIL;
+
 	return S_OK;
 }
 
