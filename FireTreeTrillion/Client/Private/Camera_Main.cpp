cdc5275c93498c746d71702e76b9a38b8a40c599
HYOSEON KIM <gktns2938@gmail.com>
2024-07-01 01:45:30 +0900

    각도수정

--- FireTreeTrillion/Client/Private/Camera_Main.cpp
+++ FireTreeTrillion/Client/Private/Camera_Main.cpp
@@ -904,6 +904,14 @@ void CCamera_Main::Make_Sequence(CAMSEQ eSeq)
 		newAction.vDir = _float3{ -.24f, 0.5f, 1.f };
 		m_CamSeq.push_back(newAction);
 
+
+		newAction = {};
+		newAction.fTime = 60.f;
+		newAction.eCamCut = CUT_HARD;
+		newAction.eCamDir = DIR_ABSOLUTE;
+		newAction.vDir = _float3{ -.24f, 0.5f, 1.f };
+		m_CamSeq.push_back(newAction);
+
 		CEffect::FX_DESC FXDesc{};
 		FXDesc.fStartDelay = { 28.f };
 		if (FAILED(CGameInstance::Get_Instance()->Add_Clone(*CGameInstance::Get_Instance()->Get_CurrentLevelID(), TEXT("Layer_Effect"), TEXT("Prototype_GameObject_TItle Logo"), &FXDesc)))
