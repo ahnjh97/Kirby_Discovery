e60d56d9685b5b1138d323c6efc21567ebb483e6
messier3331 <messier3331@naver.com>
2024-07-03 14:25:52 +0900

    덤프의 초석을 설계하였다.
    오늘부로 나는 강해질것이다.

--- FireTreeTrillion/Client/Default/CKirbyDump_State.cpp
+++ FireTreeTrillion/Client/Default/CKirbyDump_State.cpp
@@ -67,3 +67,39 @@ void CKirbyDump_Run_State::Free()
 }
 
 #pragma endregion
+
+
+#pragma region 차량 점프 상태
+
+CKirbyDump_Jump_State::CKirbyDump_Jump_State()
+{
+}
+
+void CKirbyDump_Jump_State::OnStateEnter(CModel* _pModel, _uint _iAnimIndex, _float _fAnimSpeed, _bool _bLoop, _bool _bInterpolation, _uint _iOffSet)
+{
+    __super::OnStateEnter(_pModel, _iAnimIndex, _fAnimSpeed, _bLoop, _bInterpolation, _iOffSet);
+
+}
+
+void CKirbyDump_Jump_State::OnStateUpdate(CGameObject* pGameObject, _float fTimeDelta)
+{
+
+
+}
+
+void CKirbyDump_Jump_State::OnStateExit()
+{
+}
+
+CKirbyDump_Jump_State* CKirbyDump_Jump_State::Create()
+{
+    CKirbyDump_Jump_State* pInstance = new CKirbyDump_Jump_State();
+    return pInstance;
+}
+
+void CKirbyDump_Jump_State::Free()
+{
+    __super::Free();
+}
+
+#pragma endregion
