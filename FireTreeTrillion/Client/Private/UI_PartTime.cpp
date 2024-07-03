7645f64b08f4cc75f1e26ac44355ab140c874dc9
Jiyoung_YW'sASUS <cheeze2017@naver.com>
2024-07-03 11:27:22 +0900

    디버깅용으로 바꿔놓은거 돌려놓기

--- FireTreeTrillion/Client/Private/UI_PartTime.cpp
+++ FireTreeTrillion/Client/Private/UI_PartTime.cpp
@@ -471,8 +471,8 @@ void CUI_PartTime::Compute_Timer(_float fTimeDelta)
 	m_fStandardTime += fTimeDelta;
 	if (m_fStandardTime - m_fBeforeTime >= 1.f)
 	{
-		m_fCurTime = 3.f - m_fStandardTime;
-		//m_fCurTime = 50.f - m_fStandardTime;
+		//m_fCurTime = 3.f - m_fStandardTime;
+		m_fCurTime = 50.f - m_fStandardTime;
 		if (m_fCurTime <= 0.f) m_fCurTime = 0.f;
 		Change_TimeTexures(m_fCurTime);
 		
