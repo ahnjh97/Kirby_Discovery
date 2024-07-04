3fb1e28b1309dbd026eb569d085d09fc88dd6dd8
messier3331 <messier3331@naver.com>
2024-07-04 20:19:21 +0900

    덤프 움직임 구현
    ㅇㅇ

--- FireTreeTrillion/Client/Private/FinaleKirby.cpp
+++ FireTreeTrillion/Client/Private/FinaleKirby.cpp
@@ -217,7 +217,7 @@ _float CFinaleKirby::Get_AnimTrackPosition()
 void CFinaleKirby::Bone_Rotation(_float fTimeDelta)
 {
     // 자동차일때,
-    if (INFO(m_eBodyState) == BODY_DUMPDEFAULT)
+    if (INFO(m_eBodyState) == BODY_DUMPDEFAULT && Get_State() != DUMPTSTATE_CUT)
     {
         _float fTurnAngle = -INFO(m_fMoveSpeed) * 100.f;
 
@@ -232,35 +232,19 @@ void CFinaleKirby::Bone_Rotation(_float fTimeDelta)
 
 
 
-        //pBone = m_pModelCom[INFO(m_eBodyState)]->Get_BonePtr("C_FrontBodyJ");
-        //BoneMatrix = pBone->Get_EditMatrixPtr();
+        pBone = m_pModelCom[INFO(m_eBodyState)]->Get_BonePtr("C_FrontBodyJ");
+        BoneMatrix = pBone->Get_EditMatrixPtr();
+        _float fHandleAngle = ToDegree(acos(_float4(1.f, 0.f, 0.f, 0.f).Dot(INFO(m_vHandleDir))));
 
-        //_float fHandleAngle = ToDegree(acos(_float4(1.f, 0.f,0.f,0.f).Dot(INFO(m_vHandleDir))));
-        //if (INFO(m_vTargetDir) != INFO(m_vMoveDir))
-        //{
-        //    // 좌회전
-        //    if (XMVector3Cross(INFO(m_vTargetDir), INFO(m_vMoveDir)).m128_f32[1] > 0.f)
-        //    {
-        //        fHandleAngle *= -1.f;
-        //    }
-        //}
+        // 좌회전을 한다.
+        if (XMVector3Cross(_float4(1.f, 0.f, 0.f, 0.f), INFO(m_vHandleDir)).m128_f32[1] < 0.f)
+        {
+            fHandleAngle *= -1.f;
+        }
 
-        //_float fHandleSubAngle = ToDegree(acos(INFO(m_vMoveDir).Dot(INFO(m_vHandleDir))));
-        //if (INFO(m_vMoveDir) != INFO(m_vHandleDir))
-        //{
-        //    // 우측으로 가려했다.
-        //    if (XMVector3Cross(INFO(m_vHandleDir), INFO(m_vMoveDir)).m128_f32[1] > 0.f)
-        //    {
-        //        fHandleSubAngle *= -1.f;
-        //    }
-        //}
-
-        //fHandleAngle += fHandleSubAngle;
-
-        //_float4x4 RotationMatrix = _float4x4::Identity;
-        //CUtils::Turn_OtherMatrix(RotationMatrix, _float4(0.f, 1.f, 0.f, 0.f), 1.f, fHandleAngle);
-        //*BoneMatrix = RotationMatrix;
-
+        _float4x4 RotationMatrix = _float4x4::Identity;
+        CUtils::Turn_OtherMatrix(RotationMatrix, _float4(0.f, 1.f, 0.f, 0.f), 1.f, fHandleAngle);
+        *BoneMatrix = RotationMatrix;
     }
 }
 
