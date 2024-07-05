74b7401c06cfaa07d60bf535ca8a4ba959dacdb2
ahnjh97 <ahnjh1997@naver.com>
2024-07-05 22:24:03 +0900

    심바맵 생성

--- FireTreeTrillion/Client/Private/MapToolHelper.cpp
+++ FireTreeTrillion/Client/Private/MapToolHelper.cpp
@@ -104,7 +104,7 @@ HRESULT CMapToolHelper::Initialize(void* pArg)
 	//UV 조절 가능
 	//BasicMap (UV 편집 가능)
 	m_vecMapModelNames = { "Level0Stage1Step01", "Level0Stage1Step02",  "Level1Stage1Step01", "DeeDeeDeeMap", "Town", "TownShop"
-		, "Land_VcLabo", "Land_LbLastBossBeforeStep", "LevelFinale_LbLastBuilding" };
+		, "Land_VcLabo", "Land_LbLastBossBeforeStep", "LevelFinale_LbLastBuilding", "LbBossLoom01L"};
 
 	vector<string> vecBGs = { "BG0", "BG1" };
 	m_setMapNames.insert(vecBGs.begin(), vecBGs.end());
