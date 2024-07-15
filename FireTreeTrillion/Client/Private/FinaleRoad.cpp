80e52af9e78f8dcbe2e1e8e21408fb3aa1d15228
HYOSEON KIM <gktns2938@gmail.com>
2024-07-15 15:30:59 +0900

    Update FinaleRoad.cpp

--- FireTreeTrillion/Client/Private/FinaleRoad.cpp
+++ FireTreeTrillion/Client/Private/FinaleRoad.cpp
@@ -95,7 +95,7 @@ HRESULT CFinaleRoad::Initialize(void* pArg)
 	m_bMotionBlur = false;
 	m_bStencil = true;
 	m_bRimLight = true;
-
+	m_fRimWidth = .08f;
 	m_wstrModelName = RoadDesc.wstrModelName;
 	m_pSocketMatrix = RoadDesc.pSocketMat;
 	m_bIsAnimModel = RoadDesc.bIsAnimModel;
