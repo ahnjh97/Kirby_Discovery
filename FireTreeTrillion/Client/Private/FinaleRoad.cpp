bebb235638ee77f7e15df6215467f01235263029
HYOSEON KIM <gktns2938@gmail.com>
2024-07-15 20:09:50 +0900

    레퍼런스 수정

--- FireTreeTrillion/Client/Private/FinaleRoad.cpp
+++ FireTreeTrillion/Client/Private/FinaleRoad.cpp
@@ -257,7 +257,7 @@ HRESULT CFinaleRoad::Add_Components(wstring _strModelTag, _bool _bIsAnimModel)
 
 
 	// FOR ANIMTOOL
-
+	/*
 	CHitBox::HITBOX_DESC HitBox{};
 	HitBox.pOwner = this;
 	HitBox.pDesc = &m_tColliderDesc[BODY];
@@ -267,7 +267,7 @@ HRESULT CFinaleRoad::Add_Components(wstring _strModelTag, _bool _bIsAnimModel)
 	CHECK_FAILED(hr);
 
 	Set_BodyCollider(COLLIDER_SPHERE, 0.f, 0.f, 1.f);
-
+	*/
 	return S_OK;
 }
 
