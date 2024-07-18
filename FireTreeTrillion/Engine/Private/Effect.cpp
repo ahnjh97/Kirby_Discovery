fb7c1a5c34d14cba8d97caf35d876e3ff6c4c063
HYOSEON KIM <gktns2938@gmail.com>
2024-07-18 21:23:25 +0900

    보스 이펙트 추가

--- FireTreeTrillion/Engine/Private/Effect.cpp
+++ FireTreeTrillion/Engine/Private/Effect.cpp
@@ -402,10 +402,10 @@ void CEffect::Billboard_Effect()
     m_pTransformCom->Set_State(CTransform::STATE_LOOK, XMVector3Normalize(vLook) * vScale.z);
 
     _float3 vValue = Calculate_CurValue_Lerp(0.f, KF_ROT);
-    _float fAngleZ = vValue.z * 360.f;
+    //_float fAngleZ = vValue.z * 360.f;
 
     vLook = m_pTransformCom->Get_State(CTransform::STATE_LOOK);
-    m_pTransformCom->Turn(vLook, 1.f, fAngleZ);
+    m_pTransformCom->Turn(vLook, 1.f, vValue.z);
 
 }
 
