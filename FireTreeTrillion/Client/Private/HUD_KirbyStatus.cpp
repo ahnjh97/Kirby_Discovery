03f4f8cbe84d7d4ed84e690076c0733d36a32434
runawayout <runawayout.ray@gmail.com>
2024-07-23 18:14:40 +0900

    Loading 리소스 dds 다시 전송

--- FireTreeTrillion/Client/Private/HUD_KirbyStatus.cpp
+++ FireTreeTrillion/Client/Private/HUD_KirbyStatus.cpp
@@ -131,6 +131,13 @@ _int CHUD_KirbyStatus::Tick(_float fTimeDelta)
 	if (nullptr == m_pKirby)
 		return OBJ_NOEVENT;
 
+	_float3 vNameTagScale = { 300.f, 50.f, 1.f };
+	m_pTransNameTag->Set_Scaled(vNameTagScale);
+
+	_float4 vNameTagPos = { 502.f, -394.f, 1.f, 1.f };
+	m_pTransNameTag->Set_State(CTransform::STATE_POSITION, vNameTagPos);
+
+
 	m_fTimeDelta = fTimeDelta;
 
 	_float fHpMax = { 0.f };
@@ -191,23 +198,23 @@ HRESULT CHUD_KirbyStatus::Render()
 			return S_OK;
 	}
 
-
 	if (UI_TEXTURE == m_UIObjDesc.eUIType)
 	{
 		Render_BindSet(m_pShaderCom, m_pTransformCom);
 	}
 
 	//추후 폰트가 아닌 이미지폰트로 렌더할 예정
-	//if (UI_FONT == m_UIObjDesc.eUIType)
-	//{
-	//	_float4 vFontRGBA = { m_UIObjDesc.vColorRGB.x, m_UIObjDesc.vColorRGB.y, m_UIObjDesc.vColorRGB.z, m_UIObjDesc.fAlpha * m_fAlpha };
-	//	_float2 vFontOrig = { 1.f, 1.f };
-	//	_float2 vFontScale = { 1.2f, 1.2f };
-	//	wstring wstrFontTag = { TEXT("Font_HUDSub_KR15") };
+	if (UI_FONT == m_UIObjDesc.eUIType)
+	{
+		_float4 vFontRGBA = { m_UIObjDesc.vColorRGB.x, m_UIObjDesc.vColorRGB.y, m_UIObjDesc.vColorRGB.z, m_UIObjDesc.fAlpha * m_fAlpha };
+		_float2 vFontOrig = { 1.f, 1.f };
+		_float2 vFontScale = { 1.2f, 1.2f };
+		wstring wstrFontTag = { TEXT("Font_HUDSub_KR15") };
 
-	//	m_pGameInstance->Render_Font(wstrFontTag, m_UIObjDesc.wstrText, m_vFontPos, vFontRGBA,
-	//		XMConvertToRadians(m_UIObjDesc.vDegree.z), vFontOrig, vFontScale);
-	//}
+		m_pGameInstance->Render_Font(wstrFontTag, m_UIObjDesc.wstrText, m_vFontPos, vFontRGBA,
+			XMConvertToRadians(m_UIObjDesc.vDegree.z), vFontOrig, vFontScale);
+	}
+
 #pragma region KIRBY_NAMETAG
 
 	if (nullptr == m_pKirby)
@@ -257,7 +264,8 @@ HRESULT CHUD_KirbyStatus::Render()
 	if (FAILED(m_pShaderCom->Bind_Matrix("g_ProjMatrix", &m_ProjMatrix)))
 		return E_FAIL;
 
-	hr = Bind_ShaderResources(m_pShaderCom, POSTEX_ALPHABLEND_NOTEST, m_pTexNameTag, iTexIndex);
+	//POSTEX_SOLIDBLEND_NOZTEST, POSTEX_ALPHABLEND_NOTEST
+	hr = Bind_ShaderResources(m_pShaderCom, POSTEX_SOLIDBLEND_NOZTEST, m_pTexNameTag, iTexIndex);
 	CHECK_FAILED(hr);
 
 
