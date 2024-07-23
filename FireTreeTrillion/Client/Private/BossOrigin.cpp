2c86c4d57b00681d84e866bcbd6d29a710f266df
ahnjh97 <ahnjh1997@naver.com>
2024-07-23 13:27:34 +0900

    Update BossOrigin.cpp

--- FireTreeTrillion/Client/Private/BossOrigin.cpp
+++ FireTreeTrillion/Client/Private/BossOrigin.cpp
@@ -96,24 +96,19 @@ _int CBossOrigin::Tick(_float fTimeDelta)
 
 	static _bool bOnFadeOut = false;
 	if (BO_GETOUT == m_pModelCom->Get_CurAnimIndex() && true == m_pModelCom->IsFinished())
-	{
 		m_pModelCom->Set_Animation(BO_WAIT_EYEOPEN, 60.f, false, false);
-
-		//MB(L"페이드아웃 부탁해요", L"Hey JiYoung");
-		CGameObject* pUIObj = m_pGameInstance->Get_GameObject_ByTag(LEVEL_STATIC, TEXT("Layer_ChangerUI"), TEXT("Prototype_GameObject_UI_Fading"));
-		CUI_Fading* pFadingUI = static_cast<CUI_Fading*>(pUIObj);
-		pFadingUI->Set_InOutState(CUI_Fading::FADEOUT);
-		pFadingUI->Set_IsRender(true);
-		bOnFadeOut = true;
-	}
 
 	_float fFadeOutTiming = 0.2f;
 	if (BO_WAIT_EYEOPEN == m_pModelCom->Get_CurAnimIndex() && m_pModelCom->Get_AnimRatio() < fFadeOutTiming && false == m_bFadeOut) // 지영누나 여기야 페이드아웃 부탁
 	{
 		m_bFadeOut = true;
+		CGameObject* pUIObj = m_pGameInstance->Get_GameObject_ByTag(LEVEL_STATIC, TEXT("Layer_ChangerUI"), TEXT("Prototype_GameObject_UI_Fading"));
+		CUI_Fading* pFadingUI = static_cast<CUI_Fading*>(pUIObj);
+		pFadingUI->Set_InOutState(CUI_Fading::FADEOUT);
+		pFadingUI->Set_IsRender(true);
 	}
 
-	if(bOnFadeOut)
+	if(m_bFadeOut)
 		Ready_FadeOut();
 
 	return OBJ_NOEVENT;
