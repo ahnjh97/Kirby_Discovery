8f395a95ee69247ce0e3b45134078d764df6540f
messier3331 <messier3331@naver.com>
2024-07-25 04:09:33 +0900

    비듬 관련 수정
    ㅇㅇ

--- FireTreeTrillion/Client/Private/FinalBoss.cpp
+++ FireTreeTrillion/Client/Private/FinalBoss.cpp
@@ -27,12 +27,13 @@ void CFinalBoss::Appear_Event(CGameObject* pObj)
 
 	CParticle::PARTICLE_DESC FXDesc{};
 	FXDesc.pSocketMatrix = &m_EffectSocket;
-	FXDesc.vInitPos = { 0.f, 5.f, 0.f };
-	FXDesc.fStartDelay = 1.f;
+	FXDesc.vInitPos = { 0.f, 6.f, 0.f };
+	FXDesc.fStartDelay = 2.f;
 	Add_Effect("HS_bidm C", FXDesc);
 
 	CMultiEffect::MULTI_FX_DESC MultiFXDesc{};
 	MultiFXDesc.pSocketMatrix = &m_EffectSocket;
+	MultiFXDesc.vInitPos = { 0.f, 6.f, 0.f };
 	Add_Effect("HS_orbit bidm", MultiFXDesc);
 }
 
